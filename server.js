16ca4147b33cdbf961ef7a87b9ac51a6b7c049af
ManojGowda2006 <manojgowdan807@gmail.com>
2025-02-03 14:34:00 +0530

    MongoDB client

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+require('dotenv').config();
+let a = false;
+mongoose.connect(process.env.db_url)
+.then(() => {console.log("Connected to DataBase"); a = true})
+.catch((err) => console.log(`Error while connecting ${err}`));
+
+
 
 app.get("/",(req,res)=>{
-    res.send("Hello, welcome to my ASAP Project");
+    // res.send("Hello, welcome to my ASAP Project");
+    if(a){
+        res.send("Connected to DataBase")
+    }else{
+        res.send(`Error while connecting `)
+    }
 })
 
 app.get("/ping",(req,res)=>{
