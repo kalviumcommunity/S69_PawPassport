8e4a411e34a8eb624a1ad013d70349e43497ade4
ManojGowda2006 <manojgowdan807@gmail.com>
2025-03-03 11:35:07 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css'
 import About from './pages/about.jsx'
 import Users from './components/users.jsx';
+// import Signin from './pages/signup.jsx';
+import Signup from './pages/signup.jsx';
 
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<About/>}/>
           <Route path="/read" element={<Users/>}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
     </>
